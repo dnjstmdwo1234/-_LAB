1c01cf6f73be2fa06cb8d3e02af2026f629001d8
dnjstmdwo1234 <ghu970@hanmail.net>
2019-06-01 13:09:52 +0900

    initial commit

---
+++ lab10_1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+template <typename T>
+
+class Point {
+	T x1;
+	T y1;
+	T x2;
+	T y2;
+	T r;
+public:
+	void setPointFromKeybord() {
+		cout << "첫번째 x : ";
+		cin >> x1;
+		cout << "첫번째 y : ";
+		cin >> y1;
+		cout << "두번째 x : ";
+		cin >> x2;
+		cout << "두번째 y : ";
+		cin >> y2;
+		r = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	}
+
+	void print() {
+		cout << "두점 사이의 거리 = " << r << endl;
+	}
+};
+
+int main() {
+	Point<double> p;
+	p.setPointFromKeybord();
+	p.print();
+
+	return 0;
+
+
+}
